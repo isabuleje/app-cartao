045f6b7bc4a0a4ca2fe912f0f1ea87a03b419e83
isabuleje <ibdb.snf24@uea.edu.br>
2025-06-05 10:51:33 -0400

    commit 2

---
+++ frontend/src/components/Cart2.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export default class Cart2 extends Component {
+    constructor(){
+        super()
+        this.state(
+            this.products[
+                {id:1, product_name:"Fanta", price: 4.5},
+                {id:2, product_name:"Sprite", price: 4},
+                {id:3, product_name:"Coca cola", price: 3}
+            ]
+        )
+    }
+
+
+  render() {
+    return (
+      <div className='produts'>
+        <div className='product'>
+            <div className='product_details'>
+                <h3> product</h3>
+                    <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim nemo culpa quos magni nostrum. Sapiente qui, aspernatur fugiat a eos unde quidem maiores nihil natus commodi minus recusandae neque maxime.</p>
+                    <h3>%25</h3>
+            </div>
+            <div className='product_quantity_container'>
+                <button>+</button>
+                <p>2</p>
+                <button>-</button>
+            </div>
+        </div>
+    </div>
+    )
+  }
+}
