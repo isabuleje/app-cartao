d36218e470029539772305414d1da51da0fad6ec
isabuleje <ibdb.snf24@uea.edu.br>
2025-06-12 09:42:57 -0400

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,8 @@
 import './App.css'
-import Cart from '/components/Cart.jsx'
-import Cart2 from '/components/Cart2.jsx'
-import Cart3 from '/components/Cart3.jsx'
-import Cart4 from '/components/Cart4.jsx'
-import Header from '/components/Header.jsx'
-import Footer from '/components/Footer.jsx'
 
 function App() {
   return(
-    <Cart4/>
+      <h1>aff</h1>
   )
 
 }
